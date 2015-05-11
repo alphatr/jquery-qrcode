aba8e00d94ff6e08da07d8d3ceb4bd6fac7a24df
AlphaTr <trefoiles@gmail.com>
2015-05-11 17:55:36 +0800

    add retina supports

--- src/render.canvas.js
+++ src/render.canvas.js
@@ -5,75 +5,82 @@
     }
 
     $.qrcode.Render.canvas = function (self, callback) {
-        var cfg = self.config,
+        var cfg = self.config;
 
-            i = cfg.margin,
-            j = cfg.margin,
+        var i = cfg.margin;
+        var j = cfg.margin;
 
-            mSize = cfg.moduleSize,
-            size = self.pixArr.length,
-            outSize = 2 * cfg.margin + size,
-            canvas = document.createElement('canvas'),
-            ctx = canvas.getContext('2d'),
+        var dpx  = cfg.retina ? 2 : 1;
 
-            getRGB = function (color) {
-                var red, green, blue;
-                if (color.indexOf('#') === 0) {
-                    color = color.substr(1);
+        var mSize = cfg.moduleSize * dpx;
+        var size = self.pixArr.length;
+        var outSize = 2 * cfg.margin + size;
+        var canvas = document.createElement('canvas');
+        var ctx = canvas.getContext('2d');
+
+        var getRGB = function (color) {
+            var red, green, blue;
+            if (color.indexOf('#') === 0) {
+                color = color.substr(1);
+            }
+
+            if (color.length === 6) {
+                red = color.substr(0, 2);
+                green = color.substr(2, 2);
+                blue = color.substr(4, 2);
+            } else if (color.length === 3) {
+                red = color.substr(0, 1);
+                red += red;
+                green = color.substr(1, 1);
+                green += green;
+                blue = color.substr(2, 1);
+                blue += blue;
+            } else {
+                throw ('error color');
+            }
+
+            return 'rgb(' + parseInt(red, 16) + ', ' + parseInt(green, 16) + ', ' + parseInt(blue, 16) + ')';
+        };
+
+        // 初始化画布
+        var init = function () {
+            var size = cfg.margin * 2 + self.pixArr.length;
+            canvas.width = size * mSize;
+            canvas.height = size * mSize;
+            $(canvas).css({
+                width: size * cfg.moduleSize,
+                height: size * cfg.moduleSize
+            });
+
+            ctx.fillStyle = getRGB(cfg.bgColor);
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+        };
+
+        // 设置色块
+        var setBlock = function (i, j) {
+            ctx.fillStyle = getRGB(cfg.moduleColor);
+            ctx.fillRect(i * mSize, j * mSize, mSize, mSize);
+        };
+
+        var renderLogo = function (callback) {
+            var img = new Image();
+            img.src = cfg.logo;
+            img.onload = function () {
+                var imgW = img.width,
+                    imgH = img.height,
+                    imgSize = Math.max(imgW, imgH),
+                    zoom;
+                if (imgSize > size * mSize * 0.3) {
+                    zoom = (size * mSize * 0.3) / imgSize;
+                    imgW = imgW * zoom;
+                    imgH = imgH * zoom;
                 }
-
-                if (color.length === 6) {
-                    red = color.substr(0, 2);
-                    green = color.substr(2, 2);
-                    blue = color.substr(4, 2);
-                } else if (color.length === 3) {
-                    red = color.substr(0, 1);
-                    red += red;
-                    green = color.substr(1, 1);
-                    green += green;
-                    blue = color.substr(2, 1);
-                    blue += blue;
-                } else {
-                    throw ('error color');
+                ctx.drawImage(img, (outSize * mSize - imgW) / 2, (outSize * mSize - imgH) / 2, imgW, imgH);
+                if ($.isFunction(callback)) {
+                    callback();
                 }
-
-                return 'rgb(' + parseInt(red, 16) + ', ' + parseInt(green, 16) + ', ' + parseInt(blue, 16) + ')';
-            },
-
-            // 初始化画布
-            init = function () {
-                var size = cfg.margin * 2 + self.pixArr.length;
-                canvas.width = size * mSize;
-                canvas.height = size * mSize;
-                ctx.fillStyle = getRGB(cfg.bgColor);
-                ctx.fillRect(0, 0, canvas.width, canvas.height);
-            },
-
-            // 设置色块
-            setBlock = function (i, j) {
-                ctx.fillStyle = getRGB(cfg.moduleColor);
-                ctx.fillRect(i * mSize, j * mSize, mSize, mSize);
-            },
-
-            renderLogo = function (callback) {
-                var img = new Image();
-                img.src = cfg.logo;
-                img.onload = function () {
-                    var imgW = img.width,
-                        imgH = img.height,
-                        imgSize = Math.max(imgW, imgH),
-                        zoom;
-                    if (imgSize > size * mSize * 0.3) {
-                        zoom = (size * mSize * 0.3) / imgSize;
-                        imgW = imgW * zoom;
-                        imgH = imgH * zoom;
-                    }
-                    ctx.drawImage(img, (outSize * mSize - imgW) / 2, (outSize * mSize - imgH) / 2, imgW, imgH);
-                    if ($.isFunction(callback)) {
-                        callback();
-                    }
-                };
             };
+        };
 
         init();
 
